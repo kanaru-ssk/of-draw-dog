f94a0251586d32bf7c4a20cd570f266845b0fcc8
kanaru-ssk <kanaru.ssk@gmail.com>
2024-05-28 14:10:33 +0900

    add comments

--- src/dog.cpp
+++ src/dog.cpp
@@ -25,15 +25,22 @@ void Dog::setSize(int size) { size_ = size; }
 
 void Dog::draw() {
   ofSetColor(100);
+  // face
   ofDrawCircle(x_, y_, size_ / 2);
+  // left ear
   ofDrawTriangle(x_ - size_ * 50 / 200, y_ - size_ * 130 / 200,
                  x_ - size_ * 90 / 200, y_ - size_ * 40 / 200,
                  x_ - size_ * 10 / 200, y_ - size_ * 40 / 200);
+  // the right ear
   ofDrawTriangle(x_ + size_ * 50 / 200, y_ - size_ * 130 / 200,
                  x_ + size_ * 90 / 200, y_ - size_ * 40 / 200,
                  x_ + size_ * 10 / 200, y_ - size_ * 40 / 200);
+
   ofSetColor(0);
+  // left eye
   ofDrawCircle(x_ - size_ * 35 / 200, y_ + size_ * 10 / 200, size_ / 20);
+  // right eye
   ofDrawCircle(x_ + size_ * 35 / 200, y_ + size_ * 10 / 200, size_ / 20);
+  // nose
   ofDrawCircle(x_, y_ + size_ * 30 / 200, size_ / 20);
 }
