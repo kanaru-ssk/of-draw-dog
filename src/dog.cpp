28a1a5a806f14d65f187b08e2c930157402e0bc2
kanaru-ssk <kanaru.ssk@gmail.com>
2024-05-28 15:25:24 +0900

    use const

--- src/dog.cpp
+++ src/dog.cpp
@@ -4,20 +4,28 @@ void Dog::draw() const {
   ofSetColor(100);
   // face
   ofDrawCircle(x_, y_, size_ / 2);
-  // left ear
-  ofDrawTriangle(x_ - size_ * 50 / 200, y_ - size_ * 130 / 200,
-                 x_ - size_ * 90 / 200, y_ - size_ * 40 / 200,
-                 x_ - size_ * 10 / 200, y_ - size_ * 40 / 200);
-  // the right ear
-  ofDrawTriangle(x_ + size_ * 50 / 200, y_ - size_ * 130 / 200,
-                 x_ + size_ * 90 / 200, y_ - size_ * 40 / 200,
-                 x_ + size_ * 10 / 200, y_ - size_ * 40 / 200);
+
+  // ears
+  const int EAR_TOP_OFFSET_X = size_ * 50 / 200;
+  const int EAR_TOP_OFFSET_Y = size_ * 130 / 200;
+  const int EAR_BOTTOM_OUTSIDE_OFFSET_X = size_ * 90 / 200;
+  const int EAR_BOTTOM_INSIDE_OFFSET_X = size_ * 10 / 200;
+  const int EAR_BOTTOM_OFFSET_Y = size_ * 40 / 200;
+  ofDrawTriangle(x_ - EAR_TOP_OFFSET_X, y_ - EAR_TOP_OFFSET_Y,
+                 x_ - EAR_BOTTOM_OUTSIDE_OFFSET_X, y_ - EAR_BOTTOM_OFFSET_Y,
+                 x_ - EAR_BOTTOM_INSIDE_OFFSET_X, y_ - EAR_BOTTOM_OFFSET_Y);
+  ofDrawTriangle(x_ + EAR_TOP_OFFSET_X, y_ - EAR_TOP_OFFSET_Y,
+                 x_ + EAR_BOTTOM_OUTSIDE_OFFSET_X, y_ - EAR_BOTTOM_OFFSET_Y,
+                 x_ + EAR_BOTTOM_INSIDE_OFFSET_X, y_ - EAR_BOTTOM_OFFSET_Y);
 
   ofSetColor(0);
-  // left eye
-  ofDrawCircle(x_ - size_ * 35 / 200, y_ + size_ * 10 / 200, size_ / 20);
-  // right eye
-  ofDrawCircle(x_ + size_ * 35 / 200, y_ + size_ * 10 / 200, size_ / 20);
+  // eyes
+  const int EYE_OFFSET_X = size_ * 35 / 200;
+  const int EYE_OFFSET_Y = size_ * 10 / 200;
+  const int EYE_RADIUS = size_ / 20;
+  ofDrawCircle(x_ - EYE_OFFSET_X, y_ + EYE_OFFSET_Y, EYE_RADIUS);
+  ofDrawCircle(x_ + EYE_OFFSET_X, y_ + EYE_OFFSET_Y, EYE_RADIUS);
+
   // nose
   ofDrawCircle(x_, y_ + size_ * 30 / 200, size_ / 20);
 }
